e074d41611a346f04d77e13e5066f571a60f9635
Dimitri <ganerals@gmail.com>
2014-07-16 22:55:29 +0300

    Revert "forum"
    This reverts commit bd131715baa74bc3e2a1e6182c09e171365fe289.

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-require_once('inc/classes.php');
-echo $_SESSION['id_user'];
-echo $_SESSION['username'];
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('inc/classes.php');
-$atrz=new Authorization();
-$atrz->LogOut();
-$home_url='http://'.$_SERVER['HTTP_HOST'].'/forum/index.php?';
-header('Location:'.$home_url);
